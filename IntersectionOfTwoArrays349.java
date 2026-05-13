c17bd31f10f62fa1ab80f54a9ce62e1db8b2fab7
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 23:40:53 -0400

    Create IntersectionOfTwoArrays349.java

---
+++ IntersectionOfTwoArrays349.java
@@ -0,0 +1,20 @@
+class IntersectionOfTwoArrays349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> intersection = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersection.add(num);
+            }
+        }
+        int[] result = new int[intersection.size()];
+        int index = 0;
+        for (int num : intersection) {
+            result[index++] = num;
+        }
+        return result;
+    }
+}
