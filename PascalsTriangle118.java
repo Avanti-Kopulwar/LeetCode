2e6798b394e65e046939caa65d1bb8fd70ff7eb6
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 23:31:44 -0400

    Create PascalsTriangle118.java

---
+++ PascalsTriangle118.java
@@ -0,0 +1,20 @@
+class PascalsTriangle118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new LinkedList<>();
+        List<Integer> pre = new LinkedList<>();
+        pre.add(1);
+        ans.add(pre);
+        for(int i = 1; i<numRows; i++){
+            List<Integer> sub = new LinkedList<>();
+            sub.add(1);
+            for(int j = 1; j<i; j++){
+                int val = pre.get(j)+pre.get(j-1);
+                sub.add(val);
+            }
+            sub.add(1);
+            ans.add(sub);
+            pre = sub;
+        }
+        return ans;
+    }
+}
