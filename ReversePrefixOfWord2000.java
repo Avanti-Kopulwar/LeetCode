c6aca399057b61201a9379ac4aee40396aac5e65
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 22:07:52 -0400

    Create ReversePrefixOfWord2000.java

---
+++ ReversePrefixOfWord2000.java
@@ -0,0 +1,11 @@
+class ReversePrefixOfWord2000 {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) {
+            return word;
+        }
+        String prefix = word.substring(0, index + 1);
+        String reversedPrefix = new StringBuilder(prefix).reverse().toString();
+        return reversedPrefix + word.substring(index + 1);
+    }
+}
