1e2417275fa5b264ea94319f14fb3194abe9fbcc
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 22:05:48 -0400

    Create NumberOfStringsThatAppearAsSubstringsInWord1967.java

---
+++ NumberOfStringsThatAppearAsSubstringsInWord1967.java
@@ -0,0 +1,11 @@
+class NumberOfStringsThatAppearAsSubstringsInWord1967 {
+    public int numOfStrings(String[] patterns, String word) {
+        int counter = 0;
+        for(String pattern : patterns){
+            if(word.contains(pattern)){
+                counter++;
+            }
+        } 
+        return counter;
+    }
+}
