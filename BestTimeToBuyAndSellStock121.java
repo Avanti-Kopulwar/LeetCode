d2cf5e34c235fac75c0be275a4a6fcce684a531c
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 23:34:07 -0400

    Create BestTimeToBuyAndSellStock121.java

---
+++ BestTimeToBuyAndSellStock121.java
@@ -0,0 +1,15 @@
+class BestTimeToBuyAndSellStock121 {
+    public int maxProfit(int[] prices) {
+    int min = prices[0];
+    int max = 0;
+    for (int i = 1; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i];
+        } else if (prices[i] - min > max) {
+            max = prices[i] - min;
+        }
+    }
+    return max;
+}
+
+}
