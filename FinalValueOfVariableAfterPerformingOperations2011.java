3b7f54c829f3cb955d2a8ac77655758482e39a4b
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 22:10:13 -0400

    Create FinalValueOfVariableAfterPerformingOperations2011.java

---
+++ FinalValueOfVariableAfterPerformingOperations2011.java
@@ -0,0 +1,10 @@
+class FinalValueOfVariableAfterPerformingOperations2011 {
+    public int finalValueAfterOperations(String[] operations) {
+        int sum = 0;
+        for(String s: operations){
+            if (s.charAt(1) == '+') sum++;
+            else sum--;
+        }
+        return sum;
+    }
+}
