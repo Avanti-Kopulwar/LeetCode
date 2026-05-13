de1fe6ea08310c0ef44449ad264e8ee0c5d9c1af
Avanti-Kopulwar <107633946+Avanti-Kopulwar@users.noreply.github.com>
2026-05-12 22:23:13 -0400

    Create ReverseVowelsOfString345.java

---
+++ ReverseVowelsOfString345.java
@@ -0,0 +1,24 @@
+class ReverseVowelsOfString345 {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+        while (left < right) {
+            while (left < right && !vowels.contains(chars[left])) {
+                left++;
+            }
+            while (left < right && !vowels.contains(chars[right])) {
+                right--;
+            }
+            if (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+}
